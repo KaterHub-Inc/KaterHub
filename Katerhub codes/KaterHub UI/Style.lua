b609e89a3b3fc40f54e8761ea79701172e6eafba
Kater <91840948+KATERGaming@users.noreply.github.com>
2023-07-31 20:14:07 +0200

    Create Style.lua

---
+++ Katerhub codes/KaterHub UI/Style.lua
@@ -0,0 +1,7 @@
+KaterHubWeb = {
+  SchemeColor = Color3.fromRGB(161, 131, 220),
+  Background = Color3.fromRGB(32, 32, 32),
+  Header = Color3.fromRGB(24, 24, 24),
+  TextColor = Color3.fromRGB(255, 255, 255),
+  ElementColor = Color3.fromRGB(24, 24, 24)
+}
