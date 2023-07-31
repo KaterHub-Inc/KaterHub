fab1095ac3b85d5533f1305e3f8c60bd791b4ef9
Kater <91840948+KATERGaming@users.noreply.github.com>
2023-07-31 20:36:51 +0200

    Update Style.lua

--- Katerhub codes/KaterHub UI/Style.lua
+++ Katerhub codes/KaterHub UI/Style.lua
@@ -1,5 +1,5 @@
 KaterHubWeb = {
-  SchemeColor = Color3.fromRGB(161, 131, 220),
+  SchemeColor.UIGradient_1.Color = ColorSequence.new{ColorSequenceKeypoint.new(0, Color3.new(0.494118, 0, 1)), ColorSequenceKeypoint.new(0.280277, Color3.new(0.690196, 0.388235, 1)), ColorSequenceKeypoint.new(0.544983, Color3.new(0.494118, 0, 1)), ColorSequenceKeypoint.new(0.754325, Color3.new(0.690196, 0.388235, 1)), ColorSequenceKeypoint.new(1, Color3.new(0.494118, 0, 1))},
   Background = Color3.fromRGB(32, 32, 32),
   Header = Color3.fromRGB(24, 24, 24),
   TextColor = Color3.fromRGB(255, 255, 255),
