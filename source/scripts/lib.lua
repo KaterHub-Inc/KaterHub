0c09c9743627fb2c65fda3392f19a76494b083a8
Kater <91840948+Colin4President@users.noreply.github.com>
2023-10-10 14:23:07 +0200

    Small update to the lib

--- source/scripts/lib.lua
+++ source/scripts/lib.lua
@@ -1122,7 +1122,7 @@ function KaterHubLib:Window(text)
 	DiscordInfo.Position = UDim2.new(0.304721028, 0, 0.821333349, 0)
 	DiscordInfo.Size = UDim2.new(0, 133, 0, 44)
 	DiscordInfo.Font = Enum.Font.Gotham
-	DiscordInfo.Text = "KaterHub source"
+	DiscordInfo.Text = "We introduce players to the other side of Roblox."
 	DiscordInfo.TextColor3 = Color3.fromRGB(101, 108, 116)
 	DiscordInfo.TextSize = 13.000
 	DiscordInfo.TextWrapped = true
