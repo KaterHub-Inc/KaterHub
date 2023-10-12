fdf08c41c864de7f09f955172d8860742b6aa989
Kater <91840948+Colin4President@users.noreply.github.com>
2023-10-12 23:47:19 +0200

    Update lib.lua

--- source/scripts/lib.lua
+++ source/scripts/lib.lua
@@ -20,15 +20,15 @@ end)
 
 if table.find(katerhubadmins, game.Players.LocalPlayer.UserId) then
 	pfp = userinfo["picture"] or "https://www.roblox.com/headshot-thumbnail/image?userId=".. game.Players.LocalPlayer.UserId .."&width=420&height=420&format=png"
-	user =  userinfo["user"] or game.Players.LocalPlayer.Name.." 👑"
+	user =  userinfo["user"] or game.Players.LocalPlayer.DisplayName.." 👑"
 	tag = userinfo["discriminator"] or tostring(math.random(1000,9999))
 elseif table.find(katerhubstars, game.Players.LocalPlayer.UserId) then
 	pfp = userinfo["picture"] or "https://www.roblox.com/headshot-thumbnail/image?userId=".. game.Players.LocalPlayer.UserId .."&width=420&height=420&format=png"
-	user =  userinfo["user"] or game.Players.LocalPlayer.Name.." ⭐"
+	user =  userinfo["user"] or game.Players.LocalPlayer.DisplayName.." ⭐"
 	tag = userinfo["discriminator"] or tostring(math.random(1000,9999))
 else
 	pfp = userinfo["picture"] or "https://www.roblox.com/headshot-thumbnail/image?userId=".. game.Players.LocalPlayer.UserId .."&width=420&height=420&format=png"
-	user =  userinfo["user"] or game.Players.LocalPlayer.Name
+	user =  userinfo["user"] or game.Players.LocalPlayer.DisplayName
 	tag = userinfo["discriminator"] or tostring(math.random(1000,9999))
 end
 
