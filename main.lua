12149cb39363532ed490b291ef73ae08b68bb773
Kater <91840948+Colin4President@users.noreply.github.com>
2024-12-17 17:15:00 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,2 @@
-game:GetService("StarterGui"):SetCore("SendNotification", {Title = "KaterHub v3";Text = "We working on it!\n\nCheck clipboard!";Duration = 60; Button1 = "Continue";})
+game:GetService("StarterGui"):SetCore("SendNotification", {Title = "KaterHub v3";Text = "We working on a new version (dont ask for a fucking key you idiot)\n\nCheck clipboard!";Duration = 60; Button1 = "Continue";})
 setclipboard("discord.gg/kSBmA2qKEp")
